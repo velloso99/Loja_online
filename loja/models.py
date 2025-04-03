1bd49db471363fc10c4b0f62000f964789e57f51
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-03 13:04:53 -0300

    db

--- loja/models.py
+++ loja/models.py
@@ -5,7 +5,7 @@ class produto(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     descricao = models.TextField()
     imagem = models.ImageField(upload_to='produtos/')
-    
+    link_externo = models.URLField(blank=True, null=True)
     
 
     def __str__(self):
