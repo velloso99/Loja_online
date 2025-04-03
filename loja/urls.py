225da052648c3ceacf95015fb6ff5a244f125625
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-03 13:14:50 -0300

    loja

---
+++ loja/urls.py
@@ -0,0 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = []
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- loja/views.py
+++ loja/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import produto
 
-# Create your views here.
+def index(request):
+    produtos = produto.objects.all()
+    return render(request, 'loja/index.html', {produtos: produtos})
+
